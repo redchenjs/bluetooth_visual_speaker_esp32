65c2d8d202d819ab000f8b1e9369d6cd9809040e
redchenjs <redchenjs@live.com>
2018-03-14 14:24:00 +0800

    i2s: disable apll on rev0 chips

--- main/src/device/i2s.c
+++ main/src/device/i2s.c
@@ -8,8 +8,15 @@
 #include "freertos/FreeRTOS.h"
 #include "driver/i2s.h"
 
-void i2s0_init()
-{    
+void i2s0_init(void)
+{
+    int use_apll = 0;
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    // Don't use apll on rev0 chips
+    if (chip_info.revision != 0) {
+        use_apll = 1;
+    }
     i2s_config_t i2s_config = {
         .mode = I2S_MODE_MASTER | I2S_MODE_TX,                                  // Only TX
         .sample_rate = 44100,
@@ -18,7 +25,7 @@ void i2s0_init()
         .communication_format = I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB,
         .dma_buf_count = 32,
         .dma_buf_len = 64,
-        .use_apll = 1,
+        .use_apll = use_apll,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1                                // Interrupt level 1
     };
     i2s_pin_config_t pin_config = {
