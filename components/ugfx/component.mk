1c0f8159ac9ce4144c14167dc99b0f75266184c5
redchenjs <redchenjs@live.com>
2018-03-15 12:29:33 +0800

    ugfx: change to ssd1351 driver

--- components/ugfx/component.mk
+++ components/ugfx/component.mk
@@ -1,3 +1,3 @@
-COMPONENT_SRCDIRS := src drivers/gdisp/SSD1331
+COMPONENT_SRCDIRS := src drivers/gdisp/SSD1351
 
-COMPONENT_ADD_INCLUDEDIRS := . drivers/gdisp/SSD1331
+COMPONENT_ADD_INCLUDEDIRS := . drivers/gdisp/SSD1351
