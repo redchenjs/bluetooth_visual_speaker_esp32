b2646119ecc6a3723551c4adedd5e780dc759b9d
redchenjs <redchenjs@live.com>
2018-03-15 12:22:31 +0800

    spi: add ssd1351 spi driver

--- main/src/device/spi.c
+++ main/src/device/spi.c
@@ -6,7 +6,7 @@
  */
 
 #include "driver/spi_master.h"
-#include "driver/ssd1331.h"
+#include "driver/ssd1351.h"
 
 spi_device_handle_t spi1;
 
@@ -20,19 +20,19 @@ void spi1_init(void)
         .sclk_io_num=5,
         .quadwp_io_num=-1,
         .quadhd_io_num=-1,
-        .max_transfer_sz=96*64*2
+        .max_transfer_sz=128*128*2
     };
     spi_device_interface_config_t devcfg={
-        .clock_speed_hz=26000000,               //Clock out at 26 MHz
-        .mode=0,                                //SPI mode 0
-        .spics_io_num=27,                       //CS pin
-        .queue_size=3,                          //We want to be able to queue 3 transactions at a time
-        .pre_cb=ssd1331_setpin_dc,              //Specify pre-transfer callback to handle D/C line
+        .clock_speed_hz=20000000,               // Clock out at 20 MHz
+        .mode=0,                                // SPI mode 0
+        .spics_io_num=27,                       // CS pin
+        .queue_size=6,                          // We want to be able to queue 6 transactions at a time
+        .pre_cb=ssd1351_setpin_dc,              // Specify pre-transfer callback to handle D/C line
     };
-    //Initialize the SPI bus
+    // Initialize the SPI bus
     ret=spi_bus_initialize(HSPI_HOST, &buscfg, 1);
     assert(ret==ESP_OK);
-    //Attach the LCD to the SPI bus
+    // Attach the LCD to the SPI bus
     ret=spi_bus_add_device(HSPI_HOST, &devcfg, &spi1);
     assert(ret==ESP_OK);
 }
