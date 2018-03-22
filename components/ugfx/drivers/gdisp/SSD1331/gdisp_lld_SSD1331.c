9c51fa7aa12bc8fd98ccac149ac08abc698fac58
redchenjs <redchenjs@live.com>
2018-03-22 17:53:33 +0800

    ugfx: update SSD1331 driver

--- components/ugfx/drivers/gdisp/SSD1331/gdisp_lld_SSD1331.c
+++ components/ugfx/drivers/gdisp/SSD1331/gdisp_lld_SSD1331.c
@@ -10,13 +10,13 @@
 #if GFX_USE_GDISP
 
 #if defined(GDISP_SCREEN_HEIGHT) || defined(GDISP_SCREEN_HEIGHT)
-	#if GFX_COMPILER_WARNING_TYPE == GFX_COMPILER_WARNING_DIRECT
-		#warning "GDISP: This low level driver does not support setting a screen size. It is being ignored."
-	#elif GFX_COMPILER_WARNING_TYPE == GFX_COMPILER_WARNING_MACRO
-		COMPILER_WARNING("GDISP: This low level driver does not support setting a screen size. It is being ignored.")
-	#endif
-	#undef GDISP_SCREEN_WIDTH
-	#undef GDISP_SCREEN_HEIGHT
+    #if GFX_COMPILER_WARNING_TYPE == GFX_COMPILER_WARNING_DIRECT
+        #warning "GDISP: This low level driver does not support setting a screen size. It is being ignored."
+    #elif GFX_COMPILER_WARNING_TYPE == GFX_COMPILER_WARNING_MACRO
+        COMPILER_WARNING("GDISP: This low level driver does not support setting a screen size. It is being ignored.")
+    #endif
+    #undef GDISP_SCREEN_WIDTH
+    #undef GDISP_SCREEN_HEIGHT
 #endif
 
 #define GDISP_DRIVER_VMT			GDISPVMT_SSD1331
@@ -30,16 +30,16 @@
 /*===========================================================================*/
 
 #ifndef GDISP_SCREEN_HEIGHT
-	#define GDISP_SCREEN_HEIGHT		64
+    #define GDISP_SCREEN_HEIGHT		64
 #endif
 #ifndef GDISP_SCREEN_WIDTH
-	#define GDISP_SCREEN_WIDTH		96
+    #define GDISP_SCREEN_WIDTH		96
 #endif
 #ifndef GDISP_INITIAL_CONTRAST
-	#define GDISP_INITIAL_CONTRAST	100
+    #define GDISP_INITIAL_CONTRAST	100
 #endif
 #ifndef GDISP_INITIAL_BACKLIGHT
-	#define GDISP_INITIAL_BACKLIGHT	100
+    #define GDISP_INITIAL_BACKLIGHT	100
 #endif
 
 #define GDISP_FLG_NEEDFLUSH			(GDISP_FLG_DRIVER<<0)
@@ -58,180 +58,180 @@
 /*===========================================================================*/
 
 static const uint8_t init_data[] = {
-	SSD1331_DISPLAY_OFF,
-	SSD1331_START_LINE, 0x00,
-	SSD1331_COM_OFFSET, 0x00,
-	SSD1331_PIXELS_NORMAL,
-	SSD1331_MULTIPLEX, 0x3F,
-	SSD1331_RESET, SSD1331_RESET_OFF,
-	SSD1331_POWER, SSD1331_POWER_ON,
-	SSD1331_PHASE_PERIOD, 0x31,
-	SSD1331_CLOCKS, 0xF0,
-	SSD1331_PRECHARGE_A, 0x64,
-	SSD1331_PRECHARGE_B, 0x78,
-	SSD1331_PRECHARGE_C, 0x64,
-	SSD1331_PRECHARGE_VOLTAGE, 0x3A,
-	SSD1331_DESELECT_VOLTAGE, 0x3E,
-	SSD1331_CONTRAST_A, 0x91,
-	SSD1331_CONTRAST_B, 0x50,
-	SSD1331_CONTRAST_C, 0x7D,
-	SSD1331_BRIGHTNESS, (GDISP_INITIAL_BACKLIGHT*10)/63,
-	#if GDISP_LLD_PIXELFORMAT == GDISP_PIXELFORMAT_RGB565
-		SSD1331_MODE, SSD1331_MODE_16_BIT|SSD1331_MODE_COM_SPLIT|SSD1331_MODE_COLUMN_REVERSE|SSD1331_MODE_COM_REVERSE,
-	#elif GDISP_LLD_PIXELFORMAT == GDISP_PIXELFORMAT_BGR565
-		SSD1331_MODE, SSD1331_MODE_16_BIT|SSD1331_MODE_COM_SPLIT|SSD1331_MODE_BGRSSD1331_MODE_COLUMN_REVERSE|SSD1331_MODE_COM_REVERSE,
-	#elif GDISP_LLD_PIXELFORMAT == GDISP_PIXELFORMAT_RGB332
-		SSD1331_MODE, SSD1331_MODE_8_BIT|SSD1331_MODE_COM_SPLITSSD1331_MODE_COLUMN_REVERSE|SSD1331_MODE_COM_REVERSE,
-	#elif GDISP_LLD_PIXELFORMAT == GDISP_PIXELFORMAT_BGR332
-		SSD1331_MODE, SSD1331_MODE_8_BIT|SSD1331_MODE_COM_SPLIT|SSD1331_MODE_BGRSSD1331_MODE_COLUMN_REVERSE|SSD1331_MODE_COM_REVERSE,
-	#else
-		#error "SSD1331: Invalid color format"
-	#endif
-	SSD1331_DRAW_MODE, SSD1331_DRAW_FILLRECT
+    SSD1331_DISPLAY_OFF,
+    SSD1331_START_LINE, 0x00,
+    SSD1331_COM_OFFSET, 0x00,
+    SSD1331_PIXELS_NORMAL,
+    SSD1331_MULTIPLEX, 0x3F,
+    SSD1331_RESET, SSD1331_RESET_OFF,
+    SSD1331_POWER, SSD1331_POWER_ON,
+    SSD1331_PHASE_PERIOD, 0x31,
+    SSD1331_CLOCKS, 0xF0,
+    SSD1331_PRECHARGE_A, 0x64,
+    SSD1331_PRECHARGE_B, 0x78,
+    SSD1331_PRECHARGE_C, 0x64,
+    SSD1331_PRECHARGE_VOLTAGE, 0x3A,
+    SSD1331_DESELECT_VOLTAGE, 0x3E,
+    SSD1331_CONTRAST_A, 0x91,
+    SSD1331_CONTRAST_B, 0x50,
+    SSD1331_CONTRAST_C, 0x7D,
+    SSD1331_BRIGHTNESS, (GDISP_INITIAL_BACKLIGHT*10)/63,
+    #if GDISP_LLD_PIXELFORMAT == GDISP_PIXELFORMAT_RGB565
+        SSD1331_MODE, SSD1331_MODE_16_BIT|SSD1331_MODE_COM_SPLIT|SSD1331_MODE_COLUMN_REVERSE|SSD1331_MODE_COM_REVERSE,
+    #elif GDISP_LLD_PIXELFORMAT == GDISP_PIXELFORMAT_BGR565
+        SSD1331_MODE, SSD1331_MODE_16_BIT|SSD1331_MODE_COM_SPLIT|SSD1331_MODE_BGRSSD1331_MODE_COLUMN_REVERSE|SSD1331_MODE_COM_REVERSE,
+    #elif GDISP_LLD_PIXELFORMAT == GDISP_PIXELFORMAT_RGB332
+        SSD1331_MODE, SSD1331_MODE_8_BIT|SSD1331_MODE_COM_SPLITSSD1331_MODE_COLUMN_REVERSE|SSD1331_MODE_COM_REVERSE,
+    #elif GDISP_LLD_PIXELFORMAT == GDISP_PIXELFORMAT_BGR332
+        SSD1331_MODE, SSD1331_MODE_8_BIT|SSD1331_MODE_COM_SPLIT|SSD1331_MODE_BGRSSD1331_MODE_COLUMN_REVERSE|SSD1331_MODE_COM_REVERSE,
+    #else
+        #error "SSD1331: Invalid color format"
+    #endif
+    SSD1331_DRAW_MODE, SSD1331_DRAW_FILLRECT
 };
 
 static const uint8_t gray_scale_table[] = {
-	0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0x0E, 0x10,
-	0x12, 0x15, 0x19, 0x1D, 0x21, 0x25, 0x2A, 0x30,
-	0x36, 0x3C, 0x42, 0x48, 0x50, 0x58, 0x60, 0x68,
-	0x70, 0x78, 0x82, 0x8C, 0x96, 0xA0, 0xAA, 0xB4
+    0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0x0E, 0x10,
+    0x12, 0x15, 0x19, 0x1D, 0x21, 0x25, 0x2A, 0x30,
+    0x36, 0x3C, 0x42, 0x48, 0x50, 0x58, 0x60, 0x68,
+    0x70, 0x78, 0x82, 0x8C, 0x96, 0xA0, 0xAA, 0xB4
 };
 
 LLDSPEC bool_t gdisp_lld_init(GDisplay *g) {
-	g->priv = gfxAlloc(GDISP_SCREEN_HEIGHT * GDISP_SCREEN_WIDTH * 2);
-	if (g->priv == NULL) {
-		gfxHalt("GDISP SSD1331: Failed to allocate private memory");
-	}
+    g->priv = gfxAlloc(GDISP_SCREEN_HEIGHT * GDISP_SCREEN_WIDTH * 2);
+    if (g->priv == NULL) {
+        gfxHalt("GDISP SSD1331: Failed to allocate private memory");
+    }
 
-	for(int i=0; i < GDISP_SCREEN_HEIGHT * GDISP_SCREEN_WIDTH * 2; i++) {
-		*((uint8_t *)g->priv + i) = 0x00;
-	}
+    for(int i=0; i < GDISP_SCREEN_HEIGHT * GDISP_SCREEN_WIDTH * 2; i++) {
+        *((uint8_t *)g->priv + i) = 0x00;
+    }
 
-	// Initialise the board interface
-	init_board(g);
+    // Initialise the board interface
+    init_board(g);
 
-	// Hardware reset
-	setpin_reset(g, TRUE);
-	gfxSleepMilliseconds(20);
-	setpin_reset(g, FALSE);
-	gfxSleepMilliseconds(20);
+    // Hardware reset
+    setpin_reset(g, 0);
+    gfxSleepMilliseconds(120);
+    setpin_reset(g, 1);
+    gfxSleepMilliseconds(120);
 
-	for(int i=0;i<sizeof(init_data);i++)
-		write_cmd(g, init_data[i]);
+    for(int i=0;i<sizeof(init_data);i++)
+        write_cmd(g, init_data[i]);
 
-	write_cmd(g, SSD1331_GRAYSCALE);
-	for(int i=0;i<sizeof(gray_scale_table);i++)
-		write_cmd(g, gray_scale_table[i]);
+    write_cmd(g, SSD1331_GRAYSCALE);
+    for(int i=0;i<sizeof(gray_scale_table);i++)
+        write_cmd(g, gray_scale_table[i]);
 
-	write_cmd(g, SSD1331_DISPLAY_ON);
+    write_cmd(g, SSD1331_DISPLAY_ON);
 
-	/* Initialise the GDISP structure */
-	g->g.Width = GDISP_SCREEN_WIDTH;
-	g->g.Height = GDISP_SCREEN_HEIGHT;
-	g->g.Orientation = GDISP_ROTATE_0;
-	g->g.Powermode = powerOn;
-	g->g.Backlight = GDISP_INITIAL_BACKLIGHT;
-	g->g.Contrast = GDISP_INITIAL_CONTRAST;
-	return TRUE;
+    /* Initialise the GDISP structure */
+    g->g.Width = GDISP_SCREEN_WIDTH;
+    g->g.Height = GDISP_SCREEN_HEIGHT;
+    g->g.Orientation = GDISP_ROTATE_0;
+    g->g.Powermode = powerOn;
+    g->g.Backlight = GDISP_INITIAL_BACKLIGHT;
+    g->g.Contrast = GDISP_INITIAL_CONTRAST;
+    return TRUE;
 }
 
 #if GDISP_HARDWARE_FLUSH
-	LLDSPEC void gdisp_lld_flush(GDisplay *g) {
-		if (!(g->flags & GDISP_FLG_NEEDFLUSH)) {
-			return;
-		}
-		refresh_gram(g, (uint8_t *)g->priv);
-		g->flags &= ~GDISP_FLG_NEEDFLUSH;
-	}
+    LLDSPEC void gdisp_lld_flush(GDisplay *g) {
+        if (!(g->flags & GDISP_FLG_NEEDFLUSH)) {
+            return;
+        }
+        refresh_gram(g, (uint8_t *)g->priv);
+        g->flags &= ~GDISP_FLG_NEEDFLUSH;
+    }
 #endif
 
 #if GDISP_HARDWARE_STREAM_WRITE
-	LLDSPEC	void gdisp_lld_write_start(GDisplay *g) {
-	}
-	LLDSPEC	void gdisp_lld_write_color(GDisplay *g) {
-		LLDCOLOR_TYPE c;
-		c = gdispColor2Native(g->p.color);
-		*((uint8_t *)g->priv + (g->p.y * GDISP_SCREEN_WIDTH + g->p.x) * 2 + 0) = c >> 8;
-		*((uint8_t *)g->priv + (g->p.y * GDISP_SCREEN_WIDTH + g->p.x) * 2 + 1) = c;
-	}
-	LLDSPEC	void gdisp_lld_write_stop(GDisplay *g) {
-		g->flags |= GDISP_FLG_NEEDFLUSH;
-	}
+    LLDSPEC	void gdisp_lld_write_start(GDisplay *g) {
+    }
+    LLDSPEC	void gdisp_lld_write_color(GDisplay *g) {
+        LLDCOLOR_TYPE c;
+        c = gdispColor2Native(g->p.color);
+        *((uint8_t *)g->priv + (g->p.y * g->g.Width + g->p.x) * 2 + 0) = c >> 8;
+        *((uint8_t *)g->priv + (g->p.y * g->g.Width + g->p.x) * 2 + 1) = c;
+    }
+    LLDSPEC	void gdisp_lld_write_stop(GDisplay *g) {
+        g->flags |= GDISP_FLG_NEEDFLUSH;
+    }
 #endif
 
 #if GDISP_HARDWARE_STREAM_READ
-	LLDSPEC	void gdisp_lld_read_start(GDisplay *g) {
-	}
-	LLDSPEC	color_t gdisp_lld_read_color(GDisplay *g) {
-        return (*((uint8_t *)g->priv + (g->p.y * GDISP_SCREEN_WIDTH + g->p.x) * 2 + 0) << 8) |
-               (*((uint8_t *)g->priv + (g->p.y * GDISP_SCREEN_WIDTH + g->p.x) * 2 + 1));
-	}
-	LLDSPEC	void gdisp_lld_read_stop(GDisplay *g) {
-	}
+    LLDSPEC	void gdisp_lld_read_start(GDisplay *g) {
+    }
+    LLDSPEC	color_t gdisp_lld_read_color(GDisplay *g) {
+        return (*((uint8_t *)g->priv + (g->p.y * g->g.Width + g->p.x) * 2 + 0) << 8) |
+               (*((uint8_t *)g->priv + (g->p.y * g->g.Width + g->p.x) * 2 + 1));
+    }
+    LLDSPEC	void gdisp_lld_read_stop(GDisplay *g) {
+    }
 #endif
 
 #if GDISP_NEED_CONTROL && GDISP_HARDWARE_CONTROL
-	LLDSPEC void gdisp_lld_control(GDisplay *g) {
-		switch(g->p.x) {
-		case GDISP_CONTROL_POWER:
-			if (g->g.Powermode == (powermode_t)g->p.ptr)
-				return;
-			switch((powermode_t)g->p.ptr) {
-			case powerOff:
-			case powerSleep:
-			case powerDeepSleep:
-				write_cmd(g, SSD1331_DISPLAY_OFF);
-				break;
-			case powerOn:
-				write_cmd(g, SSD1331_DISPLAY_ON);
-				break;
-			default:
-				return;
-			}
-			g->g.Powermode = (powermode_t)g->p.ptr;
-			return;
+    LLDSPEC void gdisp_lld_control(GDisplay *g) {
+        switch(g->p.x) {
+        case GDISP_CONTROL_POWER:
+            if (g->g.Powermode == (powermode_t)g->p.ptr)
+                return;
+            switch((powermode_t)g->p.ptr) {
+            case powerOff:
+            case powerSleep:
+            case powerDeepSleep:
+                write_cmd(g, SSD1331_DISPLAY_OFF);
+                break;
+            case powerOn:
+                write_cmd(g, SSD1331_DISPLAY_ON);
+                break;
+            default:
+                return;
+            }
+            g->g.Powermode = (powermode_t)g->p.ptr;
+            return;
 
-		case GDISP_CONTROL_ORIENTATION:
-			if (g->g.Orientation == (orientation_t)g->p.ptr)
-				return;
-			switch((orientation_t)g->p.ptr) {
-			case GDISP_ROTATE_0:
-				g->g.Height = GDISP_SCREEN_HEIGHT;
-				g->g.Width = GDISP_SCREEN_WIDTH;
-				break;
-			case GDISP_ROTATE_90:
-				g->g.Height = GDISP_SCREEN_WIDTH;
-				g->g.Width = GDISP_SCREEN_HEIGHT;
-				break;
-			case GDISP_ROTATE_180:
-				g->g.Height = GDISP_SCREEN_HEIGHT;
-				g->g.Width = GDISP_SCREEN_WIDTH;
-				break;
-			case GDISP_ROTATE_270:
-				g->g.Height = GDISP_SCREEN_WIDTH;
-				g->g.Width = GDISP_SCREEN_HEIGHT;
-				break;
-			default:
-				return;
-			}
-			g->g.Orientation = (orientation_t)g->p.ptr;
-			return;
+        case GDISP_CONTROL_ORIENTATION:
+            if (g->g.Orientation == (orientation_t)g->p.ptr)
+                return;
+            switch((orientation_t)g->p.ptr) {
+            case GDISP_ROTATE_0:
+                g->g.Height = GDISP_SCREEN_HEIGHT;
+                g->g.Width = GDISP_SCREEN_WIDTH;
+                break;
+            case GDISP_ROTATE_90:
+                g->g.Height = GDISP_SCREEN_WIDTH;
+                g->g.Width = GDISP_SCREEN_HEIGHT;
+                break;
+            case GDISP_ROTATE_180:
+                g->g.Height = GDISP_SCREEN_HEIGHT;
+                g->g.Width = GDISP_SCREEN_WIDTH;
+                break;
+            case GDISP_ROTATE_270:
+                g->g.Height = GDISP_SCREEN_WIDTH;
+                g->g.Width = GDISP_SCREEN_HEIGHT;
+                break;
+            default:
+                return;
+            }
+            g->g.Orientation = (orientation_t)g->p.ptr;
+            return;
 
-		case GDISP_CONTROL_BACKLIGHT:
-			if ((unsigned)g->p.ptr > 100)
-				g->p.ptr = (void *)100;
-			write_cmd(g, SSD1331_BRIGHTNESS);
-			write_cmd(g, ((unsigned)g->p.ptr*10)/63);
-			g->g.Backlight = (unsigned)g->p.ptr;
-			return;
+        case GDISP_CONTROL_BACKLIGHT:
+            if ((unsigned)g->p.ptr > 100)
+                g->p.ptr = (void *)100;
+            write_cmd(g, SSD1331_BRIGHTNESS);
+            write_cmd(g, ((unsigned)g->p.ptr*10)/63);
+            g->g.Backlight = (unsigned)g->p.ptr;
+            return;
 
-		case GDISP_CONTROL_CONTRAST:
-			return;
+        case GDISP_CONTROL_CONTRAST:
+            return;
 
-		default:
-			return;
-		}
-	}
+        default:
+            return;
+        }
+    }
 #endif
 
 #endif /* GFX_USE_GDISP */
