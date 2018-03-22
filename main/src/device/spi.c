6797c002f2198eeb477e7760ec65558af7287f8a
redchenjs <redchenjs@live.com>
2018-03-22 18:16:14 +0800

    spi: update screen driver config

--- main/src/device/spi.c
+++ main/src/device/spi.c
@@ -9,7 +9,6 @@
 #include "driver/ssd1331.h"
 #include "driver/ssd1351.h"
 #include "driver/st7735.h"
-#include "driver/st7789.h"
 
 spi_device_handle_t spi1;
 
@@ -24,41 +23,28 @@ void spi1_init(void)
         .quadwp_io_num=-1,
         .quadhd_io_num=-1,
 #if defined(CONFIG_SCREEN_PANEL_SSD1331)
-        .max_transfer_sz=96*64*2
+        .max_transfer_sz=SSD1331_SCREEN_WIDTH*SSD1331_SCREEN_HEIGHT*2
 #elif defined(CONFIG_SCREEN_PANEL_SSD1351)
-        .max_transfer_sz=128*128*2
-#elif defined(CONFIG_SCREEN_PANEL_ST7735)
-        .max_transfer_sz=160*80*2
-#elif defined(CONFIG_SCREEN_PANEL_ST7789)
-        .max_transfer_sz=240*240*2
+        .max_transfer_sz=SSD1351_SCREEN_WIDTH*SSD1351_SCREEN_HEIGHT*2
+#else
+        .max_transfer_sz=ST7735_SCREEN_WIDTH*ST7735_SCREEN_HEIGHT*2
 #endif
     };
     spi_device_interface_config_t devcfg={
-#if defined(CONFIG_SCREEN_PANEL_SSD1331)
         .clock_speed_hz=20000000,               // Clock out at 20 MHz
         .mode=0,                                // SPI mode 0
         .spics_io_num=27,                       // CS pin
+#if defined(CONFIG_SCREEN_PANEL_SSD1331)
         .queue_size=3,                          // We want to be able to queue 3 transactions at a time
         .pre_cb=ssd1331_setpin_dc,              // Specify pre-transfer callback to handle D/C line
 #elif defined(CONFIG_SCREEN_PANEL_SSD1351)
-        .clock_speed_hz=20000000,               // Clock out at 20 MHz
-        .mode=0,                                // SPI mode 0
-        .spics_io_num=27,                       // CS pin
         .queue_size=6,                          // We want to be able to queue 6 transactions at a time
         .pre_cb=ssd1351_setpin_dc,              // Specify pre-transfer callback to handle D/C line
-#elif defined(CONFIG_SCREEN_PANEL_ST7735)
-        .clock_speed_hz=20000000,               // Clock out at 20 MHz
-        .mode=0,                                // SPI mode 0
-        .spics_io_num=27,                       // CS pin
+#else
         .queue_size=6,                          // We want to be able to queue 6 transactions at a time
         .pre_cb=st7735_setpin_dc,               // Specify pre-transfer callback to handle D/C line
-#elif defined(CONFIG_SCREEN_PANEL_ST7789)
-        .clock_speed_hz=20000000,               // Clock out at 20 MHz
-        .mode=0,                                // SPI mode 0
-        .spics_io_num=27,                       // CS pin
-        .queue_size=6,                          // We want to be able to queue 6 transactions at a time
-        .pre_cb=st7789_setpin_dc,               // Specify pre-transfer callback to handle D/C line
 #endif
+        .flags=SPI_DEVICE_3WIRE | SPI_DEVICE_HALFDUPLEX
     };
     // Initialize the SPI bus
     ret=spi_bus_initialize(HSPI_HOST, &buscfg, 1);
