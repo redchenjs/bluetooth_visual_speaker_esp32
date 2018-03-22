d4cb36feb12178a1160e654e7324afa7abf15611
redchenjs <redchenjs@live.com>
2018-03-22 18:15:09 +0800

    main: rm 240x240 animations config

--- main/component.mk
+++ main/component.mk
@@ -13,4 +13,3 @@ COMPONENT_EMBED_FILES := res/snd/snd0.mp3 res/snd/snd1.mp3
 COMPONENT_EMBED_FILES += res/ani/ani0_96x64.gif res/ani/ani1_96x64.gif res/ani/ani2_96x64.gif res/ani/ani3_96x64.gif
 COMPONENT_EMBED_FILES += res/ani/ani0_128x128.gif res/ani/ani1_128x128.gif res/ani/ani2_128x128.gif res/ani/ani3_128x128.gif
 COMPONENT_EMBED_FILES += res/ani/ani0_160x80.gif res/ani/ani1_160x80.gif res/ani/ani2_160x80.gif res/ani/ani3_160x80.gif
-COMPONENT_EMBED_FILES += res/ani/ani0_240x240.gif res/ani/ani1_240x240.gif res/ani/ani2_240x240.gif res/ani/ani3_240x240.gif
