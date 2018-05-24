4a379636d3b4979870ff44df6315e17a2637cc18
redchenjs <redchenjs@live.com>
2018-05-24 10:57:37 +0800

    i2s: change interrupt level to 1

--- main/src/device/i2s.c
+++ main/src/device/i2s.c
@@ -24,7 +24,7 @@ void i2s0_init(void)
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,                           // 2-channels
         .dma_buf_count = 8,
         .dma_buf_len = 64,
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL3                                // Interrupt level 3
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1                                // Interrupt level 1
     };
     i2s_driver_install(0, &i2s_config, 0, NULL);
     i2s_pin_config_t pin_config = {
