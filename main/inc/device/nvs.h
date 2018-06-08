fb1d3c36bdfc1c8af2e7cfa7ed74fd90f6672d43
redchenjs <redchenjs@live.com>
2018-06-08 19:00:21 +0800

    device: add nvs_flash

---
+++ main/inc/device/nvs.h
@@ -0,0 +1,13 @@
+/*
+ * nvs.h
+ *
+ *  Created on: 2018-03-30 16:45
+ *      Author: Jack Chen <redchenjs@live.com>
+ */
+
+#ifndef DEVICE_NVS_H_
+#define DEVICE_NVS_H_
+
+extern void nvs_init(void);
+
+#endif /* DEVICE_NVS_H_ */
---
+++ main/src/device/nvs.c
@@ -0,0 +1,21 @@
+/*
+ * nvs.c
+ *
+ *  Created on: 2018-03-30 16:45
+ *      Author: Jack Chen <redchenjs@live.com>
+ */
+
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#define TAG "nvs"
+
+void nvs_init(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+}
