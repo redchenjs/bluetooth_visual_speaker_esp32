71da3122b03475068309a7bbe1b8fced75e7dd3b
redchenjs <redchenjs@live.com>
2018-06-08 19:09:02 +0800

    event: code format

--- main/inc/system/event.h
+++ main/inc/system/event.h
@@ -5,19 +5,19 @@
  *      Author: Jack Chen <redchenjs@live.com>
  */
 
-#ifndef INC_SYSTEM_EVENT_C_
-#define INC_SYSTEM_EVENT_C_
+#ifndef INC_SYSTEM_EVENT_H_
+#define INC_SYSTEM_EVENT_H_
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 
-enum daemon_event_group_bits {
+typedef enum daemon_event_group_bits {
     GUI_DAEMON_RELOAD_BIT  = BIT0,
     AUDIO_DAEMON_READY_BIT = BIT1
-};
+} daemon_event_group_bits_t;
 
 extern EventGroupHandle_t daemon_event_group;
 
 extern void event_init(void);
 
-#endif /* INC_SYSTEM_EVENT_C_ */
+#endif /* INC_SYSTEM_EVENT_H_ */
