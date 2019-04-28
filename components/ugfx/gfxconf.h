472e31c2717633aed2a1db31d33e0a0cd6a30b28
redchenjs <redchenjs@live.com>
2019-04-28 12:17:23 +0800

    ugfx: set flush timer to 8ms

--- components/ugfx/gfxconf.h
+++ components/ugfx/gfxconf.h
@@ -70,7 +70,7 @@
 #define GFX_USE_GDISP                                TRUE
 
 // #define GDISP_NEED_AUTOFLUSH                         TRUE
-#define GDISP_NEED_TIMERFLUSH                        10
+#define GDISP_NEED_TIMERFLUSH                        8
 // #define GDISP_NEED_VALIDATION                        TRUE
 // #define GDISP_NEED_CLIP                              TRUE
 // #define GDISP_NEED_CIRCLE                            TRUE
