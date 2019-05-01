2c8993f3a9dc1c14445c855902e17fb68b8ef05f
redchenjs <redchenjs@live.com>
2019-05-01 10:56:22 +0800

    audio_render: fix audio channel swap problem

--- main/src/user/audio_render.c
+++ main/src/user/audio_render.c
@@ -23,8 +23,8 @@ esp_err_t i2s_write_wrapper(i2s_port_t i2s_num, const void *src, size_t size, si
         uint32_t idx = 0;
         const uint8_t *data = (const uint8_t *)src;
         while (size > 0) {
-            int16_t data_l = data[idx+1] << 8 | data[idx];
-            int16_t data_r = data[idx+3] << 8 | data[idx+2];
+            int16_t data_l = data[idx+3] << 8 | data[idx+2];
+            int16_t data_r = data[idx+1] << 8 | data[idx];
             fifo_write((data_l + data_r) / 2);
             idx  += 4;
             size -= 4;
@@ -50,15 +50,15 @@ void render_sample_block(short *sample_buff_ch0, short *sample_buff_ch1, int num
     TickType_t max_wait = 20 / portTICK_PERIOD_MS; // portMAX_DELAY = bad idea
     for (int i = 0; i < num_samples; i++) {
         /* low - high / low - high */
-        const char samp32[4] = {ptr_l[0], ptr_l[1], ptr_r[0], ptr_r[1]};
+        const char samp32[4] = {ptr_r[0], ptr_r[1], ptr_l[0], ptr_l[1]}; // ESP32 CPU is Little Endian
         i2s_write_wrapper(0, (const char *)&samp32, sizeof(samp32), &bytes_written, max_wait);
 
         // DMA buffer full - retry
         if (bytes_written == 0) {
             i--;
         } else {
-            ptr_r += stride;
             ptr_l += stride;
+            ptr_r += stride;
         }
     }
 }
