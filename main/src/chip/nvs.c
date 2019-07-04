fe8928ad1cb587592de26ff421c53b24d541dfe3
redchenjs <redchenjs@live.com>
2019-07-05 00:29:40 +0800

    nvs: add ESP_ERR_NVS_NEW_VERSION_FOUND

--- main/src/chip/nvs.c
+++ main/src/chip/nvs.c
@@ -14,10 +14,11 @@
 void nvs_init(void)
 {
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
+
     ESP_LOGI(TAG, "nvs initialized.");
 }
