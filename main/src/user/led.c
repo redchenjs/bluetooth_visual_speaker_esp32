e6c4608df516b29eefcbcc1435f5706c0c5909a3
Jack Chen <redchenjs@live.com>
2020-05-02 17:43:44 +0800

    led: update gpio driver

--- main/src/user/led.c
+++ main/src/user/led.c
@@ -14,48 +14,60 @@
 #define TAG "led"
 
 #ifdef CONFIG_ENABLE_LED
-static const uint16_t led_mode_table[][2] = {
-/*  { delay, count}  */
-    {     0,     2},   // 0, Keep off
-    {  1000,     2},   // 1,
-    {   500,     2},   // 2,
-    {   250,     2},   // 3,
-    {   100,     2},   // 4,
-    {    25,     2},   // 5,
-    {    25,    25},   // 6,
-    {    25,    50},   // 7,
-    {    25,   100},   // 8,
-    {    25,     0}    // 9, Keep on
+static const TickType_t led_mode_table[][2] = {
+/*  { active, inactive }  */
+    {   2000,     2000 },   // 0
+    {   1000,     1000 },   // 1
+    {    500,      500 },   // 2
+    {    250,      250 },   // 3
+    {    100,      100 },   // 4
+    {     25,       25 },   // 5
+    {    625,       25 },   // 6
+    {   1250,       25 },   // 7
+    {   1875,       25 },   // 8
+    {   2500,       25 },   // 9
 };
 
 static uint8_t led_mode_index = 3;
 
 static void led_task(void *pvParameter)
 {
-    uint16_t i = 0;
+    bool active = false;
     portTickType xLastWakeTime;
 
-    gpio_set_direction(CONFIG_LED_PIN, GPIO_MODE_OUTPUT);
+#ifdef CONFIG_LED_ACTIVE_LOW
+    gpio_set_level(CONFIG_LED_PIN, 1);
+#else
+    gpio_set_level(CONFIG_LED_PIN, 0);
+#endif
+
+    gpio_config_t io_conf = {
+        .pin_bit_mask = BIT64(CONFIG_LED_PIN),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = false,
+        .pull_down_en = false,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&io_conf);
 
     ESP_LOGI(TAG, "started.");
 
     while (1) {
         xLastWakeTime = xTaskGetTickCount();
 
-        if (i++ % led_mode_table[led_mode_index][1]) {
 #ifdef CONFIG_LED_ACTIVE_LOW
-            gpio_set_level(CONFIG_LED_PIN, 1);
-        } else {
-            gpio_set_level(CONFIG_LED_PIN, 0);
-        }
+        if (!active) {
 #else
-            gpio_set_level(CONFIG_LED_PIN, 0);
-        } else {
-            gpio_set_level(CONFIG_LED_PIN, 1);
-        }
+        if (active) {
 #endif
+            gpio_set_level(CONFIG_LED_PIN, 1);
+        } else {
+            gpio_set_level(CONFIG_LED_PIN, 0);
+        }
 
-        vTaskDelayUntil(&xLastWakeTime, led_mode_table[led_mode_index][0] / portTICK_RATE_MS);
+        active = !active;
+
+        vTaskDelayUntil(&xLastWakeTime, led_mode_table[led_mode_index][active] / portTICK_RATE_MS);
     }
 }
 #endif
@@ -63,7 +75,7 @@ static void led_task(void *pvParameter)
 void led_set_mode(uint8_t idx)
 {
 #ifdef CONFIG_ENABLE_LED
-    if (idx >= (sizeof(led_mode_table) / 2)) {
+    if (idx >= sizeof(led_mode_table)/2) {
         ESP_LOGE(TAG, "invalid mode index");
         return;
     }
