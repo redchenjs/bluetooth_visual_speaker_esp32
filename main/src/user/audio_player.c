ddcdd12c611ddb33d5ea64c03a8e6719f66ee6ef
Jack Chen <redchenjs@live.com>
2020-05-02 17:44:14 +0800

    audio_player: code clean up

--- main/src/user/audio_player.c
+++ main/src/user/audio_player.c
@@ -121,7 +121,7 @@ static void audio_player_task(void *pvParameters)
 void audio_player_play_file(uint8_t idx)
 {
 #ifdef CONFIG_ENABLE_AUDIO_PROMPT
-    if (idx >= (sizeof(mp3_file_ptr) / 2)) {
+    if (idx >= sizeof(mp3_file_ptr)/2) {
         ESP_LOGE(TAG, "invalid file index");
         return;
     }
